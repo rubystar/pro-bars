56b3f49596f38144f7a4b634ecf4e5f0fdc516e0
Joe <joe@cloud-eight.com>
2014-07-17 01:27:54 +0100

    Revert "Updated to v1.1"
    This reverts commit 285916c5b5f6d5659f67d223a21f3845a500c1d0.

--- assets/js/pro-bars.js
+++ assets/js/pro-bars.js
@@ -1,20 +1,20 @@
-//	ProBars v1.1, Copyright 2014, Joe Mottershaw, https://github.com/joemottershaw/
+//	ProBars v1.0, Copyright 2014, Joe Mottershaw, https://github.com/joemottershaw/
 //	===============================================================================
 
 	function animateProgressBar() {
 		$('.pro-bar').each(function(i, elem) {
-			var	$elem = $(this),
-				percent = $elem.attr('data-pro-bar-percent'),
-				delay = $elem.attr('data-pro-bar-delay');
+			var	elem = $(this),
+				percent = elem.attr('data-pro-bar-percent'),
+				delay = elem.attr('data-pro-bar-delay');
 
-			if (!$elem.hasClass('animated'))
-				$elem.css({ 'width' : '0%' });
+			if (!elem.hasClass('animated'))
+				elem.css({ 'width' : '0%' });
 
-			$(elem).appear(function () {
+			if (elem.visible(true)) {
 				setTimeout(function() {
-					$elem.animate({ 'width' : percent + '%' }, 2000, 'easeInOutExpo').addClass('animated');
+					elem.animate({ 'width' : percent + '%' }, 2000, 'easeInOutExpo').addClass('animated');
 				}, delay);
-			});
+			} 
 		});
 	}
 
